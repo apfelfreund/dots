cedfc60041adc85754cca056ed564928850a4e1e
apfelfreund <60425324+apfelfreund@users.noreply.github.com>
2021-07-29 20:50:27 +0300

    Delete scripts directory

--- scripts/dwmbar.sh
+++
@@ -1,4 +0,0 @@
-while true; do
-    xsetroot -name " $( acpi | awk '{ print $4 }' ) |   $(pulsemixer -l | awk '{ print $20 }' | awk '{ print $(FNR-0) }' | sed -e "s/^.//;s/...$//"  ) | $(df -h | awk '{ print $4 }' | awk '{ print $(FNR-1) }' | sed '1d') |  $( date +"%H:%M" )"
-    sleep 10s
-done &
