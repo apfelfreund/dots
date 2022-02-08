489b7590e36fbfbec50624ef13bc946e444d7a94
apfelfreund <60425324+apfelfreund@users.noreply.github.com>
2022-02-08 13:10:13 +0300

    Update launch.sh

--- config/dwm/launch.sh
+++ config/dwm/launch.sh
@@ -1,3 +1,8 @@
+while true; do
+   xsetroot -name " $(acpi | awk '{ print $4 }') |  $( date +"%F %R" )"
+   sleep 1m    # Update time every minute
+done &
+
 setxkbmap "us,ru" "winkeys" "grp:alt_shift_toggle,grp_led:scroll" &
 picom -f &
 feh --bg-scale ~/.config/dwm/tri.jpg &
