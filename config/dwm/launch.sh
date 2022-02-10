cbf5e24483a85d66bbf11b8874f570bfa8e50dfc
apfelfreund <60425324+apfelfreund@users.noreply.github.com>
2022-02-11 01:01:29 +0300

    Update launch.sh

--- config/dwm/launch.sh
+++ config/dwm/launch.sh
@@ -3,6 +3,5 @@ while true; do
 done &
 
 setxkbmap "us,ru" "winkeys" "grp:alt_shift_toggle,grp_led:scroll" &
-picom -f &
 brightnessctl s 100 &
-feh --bg-scale ~/.config/dwm/tri.jpg &
+feh --bg-scale ~/.config/dwm/0027.jpg &
