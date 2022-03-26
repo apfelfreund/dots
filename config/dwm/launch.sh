b4eafb89d7b3a27125f107a41387e5aef222b14e
apfelfreund <60425324+apfelfreund@users.noreply.github.com>
2022-03-27 00:50:55 +0300

    Delete config/dwm directory

--- config/dwm/launch.sh
+++
@@ -1,7 +0,0 @@
-while true; do
-   xsetroot -name "  $(pamixer --get-volume) |  $(acpi | awk '{ print $4 }') |  $( date +"%F %R" ) "
-done &
-
-setxkbmap "us,ru" "winkeys" "grp:alt_shift_toggle,grp_led:scroll" &
-brightnessctl s 100 &
-feh --bg-scale ~/.config/dwm/0027.jpg &
